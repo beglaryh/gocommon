8b3f4118cfedb13901bf2c693a9c6af5e7fea480
beglaryh <harry.beglaryan@adp.com>
2023-11-15 10:48:28 -0800

    Initial commit

---
+++ optional.go
@@ -0,0 +1,44 @@
+package domain
+
+import "errors"
+
+type Optional[T any] struct {
+	value *T
+}
+
+func With[T any](t *T) (*Optional[T], error) {
+	if t == nil {
+		return nil, errors.New("nil argument passed")
+	}
+	return &Optional[T]{value: t}, nil
+}
+
+func WithNullable[T any](t *T) *Optional[T] {
+	return &Optional[T]{value: t}
+}
+
+func Empty[T any]() Optional[T] {
+	return Optional[T]{}
+}
+
+func (op *Optional[T]) IsPresent() bool {
+	return op.value != nil
+}
+
+func (op *Optional[T]) Get() T {
+	return *op.value
+}
+
+func (op *Optional[T]) IfPresent(doSomething func(t T)) {
+	if op.IsPresent() {
+		doSomething(*op.value)
+	}
+}
+
+func (op *Optional[T]) IfPresentOrElse(doSomething func(t T), orElse func()) {
+	if op.IsPresent() {
+		doSomething(*op.value)
+	} else {
+		orElse()
+	}
+}
