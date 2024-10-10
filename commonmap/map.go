f5eb5cc238414c23502e78b1c7aee2c085627ff1
Hrach Beglaryan <beglaryanh@gmail.com>
2024-10-10 12:33:41 -0700

    push map.go

---
+++ commonmap/map.go
@@ -0,0 +1,16 @@
+package commonmap
+
+import (
+	"github.com/beglaryh/gocommon/optional"
+)
+
+type Map[K comparable, V any] interface {
+	Get(k K) optional.Optional[V]
+	GetOrDefault(k K, defaultValue V) V
+	Put(k K, v V) optional.Optional[V]
+	Remove(k K) optional.Optional[V]
+	Values() []V
+	Contains(k K) bool
+	Compute(k K, func(k K, v optional.Optional[V]) optional.Optional[V]) optional.Optional[V]
+	ComputeIfAbsent(k K, func(k K, v optional.Optional[V]) optional.Optional[V]) optional.Optional[V]
+}
